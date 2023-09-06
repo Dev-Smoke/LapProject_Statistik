f6b963a5cd2cbfb73f1e7fcdeab797b5c47da97b
Dennis Olcay <ITN262232@qualifizierung.at>
2023-09-06 14:36:12 +0200

    add view in changes

--- LapProject/_SQL/changes.sql
+++ LapProject/_SQL/changes.sql
@@ -1,4 +1,19 @@
-﻿ALTER TABLE [Order]
+﻿CREATE VIEW [dbo].Top5
+AS
+SELECT        TOP (5) dbo.Product.Name, SUM(dbo.OrderLine.Amount) AS Anzahl
+FROM            dbo.OrderLine INNER JOIN
+                         dbo.Product ON dbo.OrderLine.ProductId = dbo.Product.Id
+GROUP BY dbo.Product.Name
+ORDER BY Anzahl DESC
+
+
+
+
+
+
+
+
+ALTER TABLE [Order]
 ADD Discount decimal(28,2) not null default(0)
 GO
 
